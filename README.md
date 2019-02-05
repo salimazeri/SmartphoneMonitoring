4f57113deada451149b12023caa3053a8c8a1025
salimazeri <salim.azeri@smcebi.edu.pl>
2019-02-05 15:27:31 +0100

    commit

--- README.md
+++ README.md
@@ -11,8 +11,6 @@ You also need an xampp packet:
 xampp version: XAMPP for Linux 7.3.0-0, 
 apache version: Apache/2.4.37 (Unix)
 
-1. Start xampp by "sudo ./xampp start"
-2. Start app.js by "node app.js" in project folder
-3. Create data base with localhost:3030/createdatabase
-4. Create users table with localhost:3030/createusertable
-5. It's all :), visit localhost:3030
+1. Start xampp by "sudo ./xampp start".
+2. Start app.js by "node app.js" in project folder.
+3. It's all :), visit "localhost:3030".
--- routes/index.js
+++ routes/index.js
@@ -10,18 +10,47 @@ var userTableName = 'Users';
 const db = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
+	port: databasePort,
+	database: dataBaseName
+});
+
+const db_init = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
 	port: databasePort
 });
 
-function dbconnect(){
-	if (db.state === 'disconnected'){
-		db.connect((err) =>{
-			if(err){
-				throw err;
-			}
-			console.log('Połączono z bazą danych na porcie: ' + databasePort)
-		});	
-	}		
+createDataBase();
+createUserTable();
+setTimeout(function(){connectToDB()},1000);
+
+function connectToDB(){
+	db.connect((err) =>{
+		if(err){
+			console.log(err);
+		} else {
+			console.log('Połączono z bazą danych na porcie: ' + databasePort)		
+		}
+
+	});
+}
+		
+function createDataBase(){
+	var sql = "CREATE DATABASE IF NOT EXISTS BazaDanych";
+	db_init.query(sql, function (err, result) {
+    	if (err) throw err;
+  	});
+}
+
+function createUserTable(){
+	var sql = "USE BazaDanych";
+	db_init.query(sql, function (err, result) {
+    	if (err) throw err;
+  	});
+	var sql = "CREATE TABLE IF NOT EXISTS Users (id INT(11) NOT NULL AUTO_INCREMENT, login VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY (id))";
+	db_init.query(sql, function (err, result) {
+    	if (err) throw err;
+  	});
 }
 
 var bcrypt = require('bcrypt');
@@ -45,32 +74,7 @@ var sessionChecker = (req, res, next) => {
     }    
 };
 
-router.get('/createdatabase', (req, res) => {
-	var sql = "CREATE DATABASE IF NOT EXISTS BazaDanych";
-	db.query(sql, function (err, result) {
-    	if (err) throw err;
-    	console.log("Database created");
-  	});
-  	db.database = dataBaseName;
-  	res.redirect('/');
-})
-
-router.get('/createusertable', (req, res) => {
-	var sql = "USE BazaDanych";
-	db.query(sql, function (err, result) {
-    	if (err) throw err;
-    	console.log("Using BazaDanych");
-  	});
-	var sql = "CREATE TABLE IF NOT EXISTS Users (id INT(11) NOT NULL AUTO_INCREMENT, login VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY (id))";
-	db.query(sql, function (err, result) {
-    	if (err) throw err;
-    	console.log("Table User created");
-  	});
-  	res.redirect('/');
-})
-
-router.get('/', sessionChecker, (req, res) => {
-	dbconnect();
+router.get('/', sessionChecker, (req, res) => {	
 	if (req.session.user && req.cookies.user_sid) {
 		//res.cookie('user_sid');
         res.render('index', {user: req.session.user});
